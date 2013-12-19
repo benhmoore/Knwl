89ecc34bc4a2bb5e7d39893b381b0db55d387a2a
Ben Cox <ben@benjojo.co.uk>
2013-12-19 14:02:14 +0000

    Adding tests to reflect stuff that was made in d4b0672407530bfcfaab64525997e8808a772c82

--- test/dates-spec.js
+++ test/dates-spec.js
@@ -152,4 +152,17 @@ describe("dates", function () {
     expect(output[0]).toBe(12);
   });
 
+  it("should detect dates formatted like 2013-12-15", function () {
+    x.init("I will see you on 2013-12-15");
+    var output = x.get("dates")[0]
+    expect(output[0] + "/" + output[1] + "/" + output[2]).toBe("12/15/2013");
+  });
+
+  // it("should detect dates formatted like 2014-12-15", function () {
+  //   x.init("I will see you on 2013-12-15");
+  //   var output = x.get("dates")[0]
+  //   expect(output[0] + "/" + output[1] + "/" + output[2]).toBe("12/15/2014");
+  // });
+  // This does not work Needs fixing
+
 });
