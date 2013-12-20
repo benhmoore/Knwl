ed34321eceeeec36e84fb078ae0f8485a39cdb90
Ben Cox <ben@benjojo.co.uk>
2013-12-20 10:17:18 +0000

    sanified the invalid emails as well. Also Added more fun mass tests

--- test/emails-spec.js
+++ test/emails-spec.js
@@ -16,50 +16,51 @@ var ValidEmails = [
     "test@test-corp.com"
 ];
 
+var InvalidEmails = [
+    ".@test.com",
+    "@",
+    "what",
+    "",
+    "@test.com",
+    "test@test",
+    "あいうえお@example.com"
+];
+
+
 describe("email", function() {
     for (var i = ValidEmails.length - 1; i >= 0; i--) {
-      var ve = ValidEmails[i];
-      it("should detect the valid email of " + ve, function() {
-          x.init("You can reach me on " + ve);
-          var output = x.get("emails")
-          expect(output[0][0]).toBe(ve);
-      });
+        var ve = ValidEmails[i];
+        it("should detect the valid email of " + ve, function() {
+            x.init("You can reach me on " + ve);
+            var output = x.get("emails")
+            expect(output[0][0]).toBe(ve);
+        });
     };
 
-    it("should detect more than one email", function() {
-        x.init("You can reach me on test@test.com or test@tost.com");
+    for (var i = InvalidEmails.length - 1; i >= 0; i--) {
+        var ie = InvalidEmails[i];
+        it("should not detect the email of " + ie, function() {
+            x.init("You can reach me on " + ie);
+            var output = x.get("emails")
+            expect(output.length).toBe(0);
+        });
+    };
+
+    var mailchain = ""
+    for (var i = ValidEmails.length - 1; i >= 0; i--) {
+        mailchain = mailchain + " " + ValidEmails[i];
+    };
+
+    it("Testing all valid emails in one string", function() {
+        x.init("You can reach me on " + mailchain);
         var output = x.get("emails")
-        expect(output.length).toBe(2);
+        expect(output.length).toBe(ValidEmails.length);
     });
 
-    it("should not detect emails that don't have a user in them", function() {
-        x.init("You can reach me on @test.com");
+    it("Testing all valid emails in one string comma seperated", function() {
+        x.init("You can reach me on " + ValidEmails.join());
         var output = x.get("emails")
-        expect(output.length).toBe(0);
-    });
-
-    it("should not detect emails that don't anything in them at all", function() {
-        x.init("You can reach me on @");
-        var output = x.get("emails")
-        expect(output.length).toBe(0);
-    });
-
-    it("should not detect emails that have unicode in them", function() {
-        x.init("You can reach me on あいうえお@example.com");
-        var output = x.get("emails")
-        expect(output.length).toBe(0);
-    });
-
-    it("should not detect emails that have no tld in them", function() {
-        x.init("You can reach me on what@example");
-        var output = x.get("emails")
-        expect(output.length).toBe(0);
-    });
-
-    it("should not detect emails that start with a dot", function() {
-        x.init("You can reach me on .email@example.com");
-        var output = x.get("emails")
-        expect(output.length).toBe(0);
+        expect(output.length).toBe(ValidEmails.length);
     });
 
     it("should detect if there are two emails seperated by commas", function() {
