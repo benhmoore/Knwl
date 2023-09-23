88aa966b1415a167c7c91b70053b72c7762c1cc0
Dawid Rodak <dysiek1337@gmail.com>
2023-09-23 23:57:22 +0200

    Change links.js regex

--- default_plugins/links.js
+++ default_plugins/links.js
@@ -11,7 +11,14 @@ function Links(knwl) {
 
         for (var i = 0; i < words.length; i++) {
             var word = words[i].replace(/[\(\)!]/g, ""); // replaces every bracket ')' or '(' and every '!' with an empty character
-            if (/^(https?|ftp):\/\/(-\.)?([^\s\/?\.#-]+\.?)+(\/[^\s]*)?$/i.test(word)) {
+            
+            // https://stackoverflow.com/a/3809435
+            var expression =
+                /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/gi
+            
+            var regex = new RegExp(expression)
+
+            if (regex.test(word)) {
                 var link = word;
                 if (link[link.length - 1].search(/[?.!,]/g) !== -1) {
                     link = link.substr(0, link.length-1);
